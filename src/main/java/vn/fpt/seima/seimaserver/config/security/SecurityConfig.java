586d694a49fad39772b9c46a638c25f69cbdfac7
truongnn <truongnn@9pay.vn>
2025-06-09 15:01:38 +0700

    feat : calendar transaction

--- src/main/java/vn/fpt/seima/seimaserver/config/security/SecurityConfig.java
+++ src/main/java/vn/fpt/seima/seimaserver/config/security/SecurityConfig.java
@@ -35,16 +35,23 @@ public class SecurityConfig {
                         .requestMatchers(
                                 "/",
                                 "/login",
-                                "/api/auth/otp/request",
-                                "/api/auth/otp/verify",
-                                "/api/auth/register",
+                                "/api/v1/auth/login",
+                                "/api/v1/auth/verify-otp",
+                                "/api/v1/auth/resend-otp",
+                                "/api/v1/auth/register",
+                                "/api/v1/auth/test",
+                                "/api/v1/auth/test-get",
                                 "/swagger-ui/**",
                                 "/swagger-ui.html",
                                 "/v3/api-docs/**",
+                                "/api/v1/wallets/**",
+                                "/api/v1/categories/**",
                                 "/error",
                                 "/api/v1/auth/refresh",
                                 "/api/v1/auth/google",
-                                "/api/v1/transactions/**"
+                                "/api/v1/auth/logout",
+                                "/api/v1/auth/forgot-password",
+                                "/api/v1/auth/reset-password"
                         ).permitAll()
                         .anyRequest().authenticated()
                 )
