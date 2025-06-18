f428eeb5a181db06963604f7f5d3ede0468d447c
truongnn <truongnn@9pay.vn>
2025-06-18 11:55:06 +0700

    feat : ocr service by azure

--- src/main/java/vn/fpt/seima/seimaserver/service/impl/OcrServiceImpl.java
+++ src/main/java/vn/fpt/seima/seimaserver/service/impl/OcrServiceImpl.java
@@ -1,57 +1,50 @@
 package vn.fpt.seima.seimaserver.service.impl;
 
-import com.google.api.gax.core.FixedCredentialsProvider;
-import com.google.auth.oauth2.GoogleCredentials;
-import com.google.cloud.vision.v1.*;
-import com.google.protobuf.ByteString;
+
+import lombok.AllArgsConstructor;
+import org.springframework.http.*;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 import org.springframework.web.multipart.MultipartFile;
-import vn.fpt.seima.seimaserver.config.ocr.VisionConfig;
+import vn.fpt.seima.seimaserver.config.ocr.AzureFormRecognizerConfig;
 import vn.fpt.seima.seimaserver.service.OcrService;
-import java.io.*;
-import java.util.List;
 
 @Service
+@AllArgsConstructor
 public class OcrServiceImpl implements OcrService {
 
+    private final AzureFormRecognizerConfig config;
+    private final RestTemplate restTemplate = new RestTemplate();
+    private final HttpHeaders headers = new HttpHeaders();
+    private final static String endPointUrl = "/formrecognizer/documentModels/prebuilt-invoice:analyze?api-version=2024-11-30";
     @Override
     public String extractTextFromFile(MultipartFile file) throws Exception {
 
-        VisionConfig.initGoogleCredentials();
+        String analyzeUrl = config.getEndpoint() +endPointUrl;
 
-        String credentialsPath = System.getProperty("GOOGLE_APPLICATION_CREDENTIALS");
-        GoogleCredentials credentials;
-        try (FileInputStream credStream = new FileInputStream(credentialsPath)) {
-            credentials = GoogleCredentials.fromStream(credStream);
-        }
+        headers.set("Ocp-Apim-Subscription-Key", config.getApiKey());
+        headers.setContentType(MediaType.APPLICATION_OCTET_STREAM);
 
-        // 3. Tạo ImageAnnotatorClient với credentials thủ công
-        ImageAnnotatorSettings settings = ImageAnnotatorSettings.newBuilder()
-                .setCredentialsProvider(FixedCredentialsProvider.create(credentials))
-                .build();
+        HttpEntity<byte[]> request = new HttpEntity<>(file.getBytes(), headers);
+        ResponseEntity<Void> response = restTemplate.exchange(analyzeUrl, HttpMethod.POST, request, Void.class);
 
-        try (ImageAnnotatorClient vision = ImageAnnotatorClient.create(settings)) {
-            ByteString imgBytes = ByteString.readFrom(file.getInputStream());
+        String operationLocation = response.getHeaders().getFirst("Operation-Location");
+        if (operationLocation == null) throw new IllegalStateException("Missing Operation-Location header");
 
-            Image img = Image.newBuilder().setContent(imgBytes).build();
-            Feature feature = Feature.newBuilder().setType(Feature.Type.DOCUMENT_TEXT_DETECTION).build();
+        Thread.sleep(3000);
 
-            AnnotateImageRequest request = AnnotateImageRequest.newBuilder()
-                    .addFeatures(feature)
-                    .setImage(img)
-                    .build();
+        HttpHeaders resultHeaders = new HttpHeaders();
+        resultHeaders.set("Ocp-Apim-Subscription-Key", config.getApiKey());
 
-            List<AnnotateImageResponse> responses = vision.batchAnnotateImages(List.of(request)).getResponsesList();
+        HttpEntity<Void> resultRequest = new HttpEntity<>(resultHeaders);
+        ResponseEntity<String> resultResponse = restTemplate.exchange(
+                operationLocation,
+                HttpMethod.GET,
+                resultRequest,
+                String.class
+        );
 
-            for (AnnotateImageResponse res : responses) {
-                if (res.hasError()) {
-                    throw new IOException("Lỗi từ Vision API: " + res.getError().getMessage());
-                }
-                return res.getFullTextAnnotation().getText();
-            }
-        }
-
-        return "Không có văn bản nào được nhận diện.";
+        return resultResponse.getBody();
     }
 
 }
