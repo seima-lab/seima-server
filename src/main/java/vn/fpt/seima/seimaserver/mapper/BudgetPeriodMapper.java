441b259c2b2429fbb0c8531c58d49a1b2a12f370
andt204 <dta2042003@gmail.com>
2025-08-10 11:25:56 +0700

    feat: add field budgetName for Budget Period

--- src/main/java/vn/fpt/seima/seimaserver/mapper/BudgetPeriodMapper.java
+++ src/main/java/vn/fpt/seima/seimaserver/mapper/BudgetPeriodMapper.java
@@ -9,6 +9,7 @@ import vn.fpt.seima.seimaserver.entity.BudgetPeriod;
 @Mapper(componentModel = "spring")
 public interface BudgetPeriodMapper {
     @Mapping(target = "status", source = "status")
+    @Mapping(target = "budgetName", source = "budget.budgetName")
     BudgetPeriodResponse toResponse(BudgetPeriod entity);
     @Mapping(target = "status", source = "status")
     BudgetLastResponse toLastResponse(BudgetPeriod entity);
